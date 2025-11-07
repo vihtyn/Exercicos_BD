38192d1c217a03954165d0387eab0279a8aa18c5
Victor Caldas Nery <167125772+vihtyn@users.noreply.github.com>
2025-11-07 17:29:55 -0300

    Add files via upload

---
+++ Atividade_N2_BD.sql
@@ -0,0 +1,8 @@
+/*Atividade:
+
+/* Questão 1:
+
+/* Não não foi a melhor forma de popular, porque no caso para cada inserção for criado um insert diferente
+/* o que deixa o processo bem mais lento. Uma forma que talvez diminuiria o tempo para os dados serem inseridos
+/* seria agrupar os dados nos VALUES para cada tabela que o dado for inserido.
+
