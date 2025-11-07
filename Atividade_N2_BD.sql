62831464fd33245aad743d964a263f494ac45ad8
Victor Caldas Nery <167125772+vihtyn@users.noreply.github.com>
2025-11-07 17:32:57 -0300

    Add files via upload

--- Atividade_N2_BD.sql
+++ Atividade_N2_BD.sql
@@ -4,5 +4,7 @@
 
 /* Não não foi a melhor forma de popular, porque no caso para cada inserção for criado um insert diferente
 /* o que deixa o processo bem mais lento. Uma forma que talvez diminuiria o tempo para os dados serem inseridos
-/* seria agrupar os dados nos VALUES para cada tabela que o dado for inserido.
+/* seria agrupar os dados nos VALUES para cada tabela que o dado for inserido. E para aprimorar o script, poderiam 
+/* ser usados comandos TCL, como START TRANSACTION que inicia o processo, COMMIT para confirmar as alterações feitas
+/* e ROLLBACK para desfazer o porcesso caso algo de errado no meio do caminho.
 
